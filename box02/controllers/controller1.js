34dca3106494f21d144f9d7bf806aebba84a5422
benjirez <benjirez@gmail.com>
2018-03-02 15:42:16 -0800

    box01 and box02 workin

---
+++ box02/controllers/controller1.js
@@ -0,0 +1,39 @@
+var dbcreds = require('../../dbcreds');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extend: false});
+
+mongoose.connect('mongodb://'+ dbcreds.user +':' + dbcreds.pw + '@ds051913.mlab.com:51913/rezdb');
+var model1 = mongoose.model('rez2_collection2', mongoose.Schema({item: String}) ); //todoModel
+//var data = [{item: 'get blue milk'}, {item: "re-attach 3PO's arm"}, {item: 'pickup power converters at toshi station'}];
+
+module.exports = function(xsvr){
+
+	// get todo data and render view
+	xsvr.get('/', function(req, res){
+		var data = model1.find({}, function(err, data){
+			if (err) throw err;
+			//console.log('into the shoot flyboy');
+			res.render('view1', {todos: data});
+		});
+
+		console.log('controller1 called');
+	});
+
+	//create new todoModel with data from req.body, push to db, reload view
+	xsvr.post('/todo', urlencodedParser, function(req, res){
+		var newTodo = model1(req.body).save(function(err, data){
+			if (err) throw err;
+			res.json(data);
+		});
+	});
+
+	xsvr.delete('/todo/:item', function(req, res){
+		//delete requested item from db
+		model1.find( {item: req.params.item.replace(/\-/g, " ")} ).remove(function(err, data){
+			if (err) throw err;
+			res.json(data);
+		});
+	});
+
+};
