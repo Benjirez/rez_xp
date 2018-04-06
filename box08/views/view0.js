0131c318293927de24e239d52ff03e37bd063489
benjirez <benjirez@gmail.com>
2018-04-05 17:49:51 -0700

    iterating query object json

---
+++ box08/views/view0.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+	$('#form1').on('submit', function(){
+		console.log('submitting yo');
+    var newItem = $('#form1 input');
+    var item = { a1: [1,2,{a2: [3,4,{a3: [5,6,{a4: [7,8,{a5: [9,10,{a6: "deep"}] }] }] }] }] }; //p1: newItem.val(),
+
+    $.ajax({
+        type: 'POST',
+        url: '/sch',
+        data: item,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+    });
+
+    return false;
+
+  });
+
+});
